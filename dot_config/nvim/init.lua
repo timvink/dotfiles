1132e956d33d9ffc42e4322b5ea332e84b4797c3
Tim Vink <vinktim@gmail.com>
2025-09-06 23:27:16 +0200

    Add setup scripts and more configs

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -15,3 +15,12 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up half page and center
 -- Remap Y to yank from the cursor to the end of the line.
 -- This makes Y consistent with C and D.
 vim.keymap.set('n', 'Y', 'y$', { desc = 'Yank to end of line' })
+
+-- Use smart indentation
+vim.opt.smartindent = true
+
+-- Relative line numbers, for quick jumps with j/k
+vim.opt.relativenumber = true
+
+-- More settings to look at:
+-- https://github.com/ThePrimeagen/init.lua/blob/78fe7448e98707d1e787ed5e8ef03367132fc103/lua/theprimeagen/set.lua#L11
